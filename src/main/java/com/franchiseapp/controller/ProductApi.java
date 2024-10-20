ddd33604bb4da419d5e61d6d2ab17d78b7d27831
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-20 10:42:27 -0500

    Implemented logic behind the app impl

--- src/main/java/com/franchiseapp/controller/ProductApi.java
+++ src/main/java/com/franchiseapp/controller/ProductApi.java
@@ -7,6 +7,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequestMapping(ApiPathConstants.V1_ROUTE + ApiPathConstants.PRODUCT)
 public interface ProductApi {
 
@@ -14,16 +16,20 @@ public interface ProductApi {
     @Operation(summary = "Create a new product for a branch based on its id")
     ResponseEntity<ProductModel> createProduct(@PathVariable Long branchId, @RequestBody ProductModel productModel);
 
-    @PutMapping("/{productId}")
+    @PutMapping("/stock/{productId}")
     @Operation(summary = "Update the stock of a product")
     ResponseEntity<ProductModel> updateProductStock(@PathVariable Long productId, @RequestParam Integer stock);
 
-    @PutMapping("/{productId}")
+    @PutMapping("/name/{productId}")
     @Operation(summary = "Update the name of a product")
     ResponseEntity<ProductModel> updateProductName(@PathVariable Long productId, @RequestBody UpdateNameDTO updateNameDTO);
 
     @DeleteMapping("/{productId}")
     @Operation(summary = "Delete a product")
     ResponseEntity<Void> deleteProduct(@PathVariable Long productId);
+
+    @GetMapping("/most-stock/{franchiseId}")
+    @Operation(summary = "Get products with the most stock for a specific franchise")
+    ResponseEntity<List<ProductModel>> getProductsWithMostStockByFranchise(@PathVariable Long franchiseId);
 
 }
